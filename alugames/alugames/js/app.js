b92408b8d4cb652ff3b37275450af2a28ad662d7
Yanngc32 <102327427+Yanngc32@users.noreply.github.com>
2023-12-11 18:10:06 -0300

    Add files via upload

---
+++ alugames/alugames/js/app.js
@@ -0,0 +1,16 @@
+
+function alterarStatus(id){
+    let jogoClicado = document.getElementById(`game-${id}`);
+    let imagemJogo = jogoClicado.querySelector(".dashboard__item__img");
+    let botao = jogoClicado.querySelector(".dashboard__item__button");
+
+    if (imagemJogo.classList.contains('dashboard__item__img--rented')){
+        imagemJogo.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        imagemJogo.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+}
